ff43015aa198d3edc93779a1b783e25109f3b717
Sergey Chernobrivenko <ser4@me.com>
2014-02-04 09:45:37 +0400

    first

---
+++ LoginApp/src/ru/chernobrivenko/loginapp/LoginServlet.java
@@ -0,0 +1,36 @@
+package ru.chernobrivenko.loginapp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ru.chernobrivenko.loginapp.service.LoginService;
+
+
+@WebServlet(description = "Login servlet", urlPatterns ="/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId, password;
+		
+		userId = request.getParameter("userId");
+		password = request.getParameter("password");
+		
+		LoginService loginService = new LoginService();
+		
+		if(loginService.authenticate(userId, password))
+		{
+			request.getSession().setAttribute("userId", userId);
+			response.sendRedirect("success.jsp");
+		}
+		else
+			response.sendRedirect("login.jsp");
+	}
+
+}
---
+++ LoginApp/src/ru/chernobrivenko/loginapp/service/LoginService.java
@@ -0,0 +1,11 @@
+package ru.chernobrivenko.loginapp.service;
+
+public class LoginService {
+	public boolean authenticate(String userId, String password)
+	{
+		if(password == null || password.trim() == "")
+			return false;
+		else
+			return true;
+	}
+}
